0ad39b3c14fc85f220826a776da24fc26c520a58
Ian <netbug@cox.net>
2014-01-19 23:11:47 -0800

    did some formatting, and allowed for offline testing.

--- main.cpp
+++ main.cpp
@@ -2,25 +2,34 @@
 #include "stdafx.h"
 #include <opencv\cv.h>
 #include <exception>
-#include <assert.h>
 using namespace cv;
 using namespace std;
 
+//#define LIVE_CAP //uncomment for axis camera use
+
 int main( int argc, char** argv )
 {
-    
+
 	namedWindow( "Display Window", WINDOW_AUTOSIZE ); // Create a window for display.
 	namedWindow( "Input", WINDOW_AUTOSIZE);
 	namedWindow("intermediate", WINDOW_AUTOSIZE);
-    Mat image;
+	Mat image;
 
+#ifdef LIVE_CAP
 	VideoCapture cap("http://10.16.22.11/mjpg/video.mjpg");
 	try{
-    while(cap.isOpened())
-	{
+		while(cap.isOpened())
+		{
+#else 
+	try{
+#endif
 		Mat in=Mat::zeros(image.rows, image.cols, CV_8UC3);
+#ifdef LIVE_CAP
 		cap.grab();
 		cap.retrieve(in);
+#else
+		in = imread("cap_sample.jpg");
+#endif
 		Mat image;
 		Mat image_bw;
 		cvtColor(in, image_bw, CV_RGB2GRAY);
@@ -28,7 +37,7 @@ int main( int argc, char** argv )
 		adaptiveThreshold(image_bw, image, 245, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 7, -5);
 		imshow("intermediate", image);
 		Mat dest =Mat::zeros(in.size(), CV_32FC3);
-		
+
 		vector<vector<Point> > contours;
 		vector<Vec4i> hierarchy;
 		findContours(image, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_TC89_KCOS);
@@ -40,18 +49,21 @@ int main( int argc, char** argv )
 				contours.erase(contours.begin()+i);
 			}
 		}
+#ifdef LIVE_CAP
 		if(waitKey(30) >=0) break;
+
 	}
-	}
-	catch (cv::Exception& e) {
-		cout <<"CV_ERROR: " << e.what();
-	}
-	catch (std::exception& e) {
-		cout << "ERROR: " << e.what();
-		
-	}
-	
-	
-    waitKey(0);
-    return 0;
+#endif
+}
+catch (cv::Exception& e) {
+	cout <<"CV_ERROR: " << e.what();
+}
+catch (std::exception& e) {
+	cout << "ERROR: " << e.what();
+
+}
+
+
+waitKey(0);
+return 0;
 }
