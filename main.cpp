1ba0ff187268ad817a2829e82f90c289b0bd64f6
unknown <ianc_p@FRCPROGRAMMING4.phs-3d.Engineering>
2014-01-19 19:07:18 -0800

    image threshold kinda working

--- main.cpp
+++ main.cpp
@@ -1,23 +1,55 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <iostream>
 
+#include "stdafx.h"
+#include <opencv\cv.h>
+#include <exception>
+#include <assert.h>
 using namespace cv;
 using namespace std;
 
 int main( int argc, char** argv )
 {
     
-	namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
+	namedWindow( "Display Window", WINDOW_AUTOSIZE ); // Create a window for display.
+	namedWindow( "Input", WINDOW_AUTOSIZE);
+	namedWindow("intermediate", WINDOW_AUTOSIZE);
     Mat image;
+
 	VideoCapture cap("http://10.16.22.11/mjpg/video.mjpg");
+	try{
     while(cap.isOpened())
 	{
-		cap>>image;
+		Mat in=Mat::zeros(image.rows, image.cols, CV_8UC3);
+		cap>>in;
+		Mat image;
+		Mat image_bw;
+		cvtColor(in, image_bw, CV_RGB2GRAY);
+		adaptiveThreshold(image_bw, image, 190, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, -10);
 		if(image.empty()) break;
-		imshow( "Display window", image ); // Show our image inside it.
+
+		Mat dest = Mat::zeros(image.rows, image.cols, CV_8UC3);
+		image=image > 1;
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+		findContours(image, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+		
+
+		for (int i = 0; i>=0; i=hierarchy[i][0])
+		{
+			Scalar color(rand()&255, rand()&255, rand()&255);
+			drawContours( dest, contours, i, color, CV_FILLED, 8, hierarchy);
+		}
+		
+		imshow("Display window", image); // Show our image inside it.
+		imshow("Input", in);
+		imshow("intermediate", image_bw);
 		if(waitKey(30) >=0) break;
 	}
+	}
+	catch (std::exception& e) {
+		cout << e.what();
+		return 1;
+	}
+	
     waitKey(0);
     return 0;
 }
