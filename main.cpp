57a2de20baa83e66673693cfe77b27e2c8ede521
unknown <ianc_p@FRCPROGRAMMING4.phs-3d.Engineering>
2014-01-21 18:33:06 -0800

    Revert "Code is still pretty ugly, but it's still working."
    This reverts commit 7b8c62d65df6e27f1cc042ec6a27b569bff8282b.
    
    Conflicts:
    	main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include "stdafx.h"
 #include <opencv\cv.h>
 #include <exception>
-#include "Vision.h"
 using namespace cv;
 using namespace std;
 #define R_ 2
@@ -25,10 +24,9 @@ int main( int argc, char** argv )
 {
 	int counter = 0;
 	namedWindow( "contour", WINDOW_AUTOSIZE);
-#ifdef _DEBUG
 	namedWindow( "Input-b1", WINDOW_AUTOSIZE);
 	namedWindow( "Input-b2", WINDOW_AUTOSIZE);
-#endif
+
 #ifdef LIVE_CAP
 	VideoCapture cap("http://10.16.22.11/mjpg/video.mjpg");
 	while(cap.isOpened())
@@ -46,42 +44,89 @@ int main( int argc, char** argv )
 	}
 #else
 	in = imread("image.jpg");
-	Vision v(in);
 #endif
-	v.threshold();
-	v.processContours();
-	v.filterContours();
+	vector<Mat> images;
+	split(in, images);
+	imshow("Input-b1", images[B_]);
 
-	Mat drawing = Mat::zeros( v.images[B_].size(), CV_8UC3 );
-	for(unsigned int i = 0; i< v.contours.size(); i++ )
+	//still debating about wether to use normal or adaptive thresholds.
+	adaptiveThreshold(images[B_], images[B_], 245, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 21, -10);
+	//threshold(images[B_], images[B_], 230, 255, THRESH_BINARY);
+
+	imshow("Input-b2", images[B_]);
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	vector<KeyPoint> centers;
+	vector<Point2f> rCenters;
+	const int centerX = in.cols/2;
+	const int centerY = in.rows/2;
+	int thresh = 100;
+	Canny( images[B_], images[B_], thresh, thresh*2, 3 ); //edge detection
+
+	// Find contours
+	findContours( images[B_], contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+	
+	vector<vector<Point> > contours_poly( contours.size() );
+	vector<Rect> boundRect( contours.size() );
+	vector<Point2f>center( contours.size() );
+
+	// Draw contours
+	
+	for(unsigned int j =0; j < contours.size(); j++) // trim the extraneous contours
 	{
-		drawContours( in, v.contours, i, Scalar( 255, 0,0 ), 1, 8,v.hierarchy, 0, Point() );
-		rectangle( in, v.boundRect[i].tl(), v.boundRect[i].br(), Scalar(0,255,0), 1, 8, 0 );
-		v.centers.push_back(
+		while((contours[j].size() <= 4 )&&(j < contours.size())) { //if chain contains less than 4 points				
+			contours.erase(contours.begin()+j);
+			hierarchy.erase(hierarchy.begin()+j);
+		}
+	}
+
+	for(unsigned int i = 0; i < contours.size(); i++ )
+	{ 
+		approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+		boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+	}
+
+	for(unsigned int j =0; j < contours.size(); j++) // trim the extraneous bounding boxes
+	{
+		while(((5000.0f<boundRect[j].area())||(2000.0f>boundRect[j].area()))&&(j < contours.size())) { //a sort of band pass filter for rectangle area.			
+			contours.erase(contours.begin()+j);
+			hierarchy.erase(hierarchy.begin()+j);
+			boundRect.erase(boundRect.begin()+j);
+		}
+	}
+	Mat drawing = Mat::zeros( images[B_].size(), CV_8UC3 );
+	for(unsigned int i = 0; i< contours.size(); i++ )
+	{
+		drawContours( in, contours, i, Scalar( 255, 0,0 ), 1, 8, hierarchy, 0, Point() );
+		rectangle( in, boundRect[i].tl(), boundRect[i].br(), Scalar(0,255,0), 1, 8, 0 );
+		centers.push_back(
 			KeyPoint(
-			((float)(v.boundRect[i].tl().x + v.boundRect[i].br().x) / 2),
-			((float)(v.boundRect[i].tl().y + v.boundRect[i].br().y) / 2),
+			((float)(boundRect[i].tl().x + boundRect[i].br().x) / 2),
+			((float)(boundRect[i].tl().y + boundRect[i].br().y) / 2),
 			1.0f)
 			);
-		drawKeypoints(in, v.centers, drawing, Scalar(0,0,255));
+		drawKeypoints(in, centers, drawing, Scalar(0,0,255));
 	}
 	
 
 	
-	for(unsigned int i = 0; i<v.centers.size(); i++)
+	for(unsigned int i = 0; i<centers.size(); i++)
 	{
-		v.rCenters.push_back(Point2f(
-			((v.centers[i].pt.x-v.centerX)*(2/(float)in.cols)), 
-			((v.centers[i].pt.y-v.centerY)*(2/(float)in.rows))));
+		rCenters.push_back(Point2f(
+			((centers[i].pt.x-centerX)*(2/(float)in.cols)), 
+			((centers[i].pt.y-centerY)*(2/(float)in.rows))));
 	}
 	cout<<"rows: " << in.rows << " cols: " << in.cols<<endl;
 	cout<<"[ID] [CenterX,CenterY] [Apparent Size (px.)] [relative position to center]"<<endl;
-	for(unsigned int i = 0; i< v.centers.size(); i++)
+	for(unsigned int i = 0; i< centers.size(); i++)
 	{
-		cout << "[" << i << "] " << v.centers[i].pt <<" ["<<v.boundRect[i].area()<<"] "<<v.rCenters[i]<<endl;
+		cout << "[" << i << "] " << centers[i].pt <<" ["<<boundRect[i].area()<<"] "<<rCenters[i]<<endl;
 	}
-	imshow("contour", drawing);
+
+imshow("contour", drawing);
 	v.~Vision();
+
 
 #ifdef LIVE_CAP
 	if(waitKey(30) >=0) break;
