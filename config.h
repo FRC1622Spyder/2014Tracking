59509701d80c5993f20d7aca6187d43d061243bc
Ian <netbug@cox.net>
2014-03-26 11:40:16 -0700

    Shrank the message size again, as we likely won't ever see a billion objects. If we do manage to do it though, the first two parts of the first triad will read 'ID10T'.

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 #pragma once
 
-//#define LIVE_CAP //uncomment for axis camera use
-#define TARGET_IP "192.168.1.143"
-#define EXTRA_OBJECTS //this is for debuging; when you want a variet of extraneous data.
+#define LIVE_CAP //uncomment for axis camera use
+#define TARGET_IP "10.16.22.2"
+//#define EXTRA_OBJECTS //this is for debuging; when you want a variet of extraneous data.
